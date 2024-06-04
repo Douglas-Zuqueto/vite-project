818e73014f184c5256e3255d43edd52dd0ab746d
Douglas-Zuqueto <0001039234@senaimgaluno.com.br>
2024-06-03 22:25:28 -0300

    jpeg para jpg

--- src/Componentes/dataMenu.js
+++ src/Componentes/dataMenu.js
@@ -20,49 +20,49 @@ export const responsive = {
 export const menuHamburguer = [
     {
       id: 1,
-      imageurl: "../assets/Img/ham1.jpeg",
+      imageurl: "../assets/Img/ham1.jpg",
       name: "Hambúrguer Clássico",
       price: "R$18,99"
   },
   {
       id: 2,
-      imageurl: "../assets/Img/ham2.jpeg",
+      imageurl: "../assets/Img/ham2.jpg",
       name: "Hambúrguer com 3 Bifes, Cheddar e Bacon",
       price: "R$25,99"
   },
   {
       id: 3,
-      imageurl: "../assets/Img/ham3.jpeg",
+      imageurl: "../assets/Img/ham3.jpg",
       name: "Hambúrguer Simples com Cebola Roxa",
       price: "R$15,99"
   },
   {
       id: 4,
-      imageurl: "../assets/Img/ham4.jpeg",
+      imageurl: "../assets/Img/ham4.jpg",
       name: "Hambúrguer com Dois Bifes, Cheddar e Picles",
       price: "R$22,50"
   },
   {
       id: 5,
-      imageurl: "../assets/Img/ham5.jpeg",
+      imageurl: "../assets/Img/ham5.jpg",
       name: "Hambúrguer Artesanal",
       price: "R$30,00"
   },
   {
       id: 6,
-      imageurl: "../assets/Img/ham6.jpeg",
+      imageurl: "../assets/Img/ham6.jpg",
       name: "Hambúrguer com Dois Bifes, Bacon e Cheddar",
       price: "R$28,50"
   },
   {
       id: 7,
-      imageurl: "../assets/Img/ham7.jpeg",
+      imageurl: "../assets/Img/ham7.jpg",
       name: "Hambúrguer Gourmet",
       price: "R$35,00"
   },
   {
       id: 8,
-      imageurl: "../assets/Img/ham8.jpeg",
+      imageurl: "../assets/Img/ham8.jpg",
       name: "Rosquinha de Hambúrguer com Bacon, Ovo e Cheddar",
       price: "R$20,00"
   }
@@ -70,49 +70,49 @@ export const menuHamburguer = [
 export const menuHotD = [
     {
       id: 1,
-      imageurl: "../assets/Img/hot1.jpeg",
+      imageurl: "../assets/Img/hot1.jpg",
       name: "Cachorro Quente com Bastante Molho",
       price: "R$12,99"
   },
   {
       id: 2,
-      imageurl: "../assets/Img/hot2.jpeg",
+      imageurl: "../assets/Img/hot2.jpg",
       name: "Cachorro Quente com Molho Vermelho",
       price: "R$13,99"
   },
   {
       id: 3,
-      imageurl: "../assets/Img/hot3.jpeg",
+      imageurl: "../assets/Img/hot3.jpg",
       name: "Cachorro Quente com Palmito",
       price: "R$14,99"
   },
   {
       id: 4,
-      imageurl: "../assets/Img/hot4.jpeg",
+      imageurl: "../assets/Img/hot4.jpg",
       name: "Cachorro Quente Simples",
       price: "R$10,99"
   },
   {
       id: 5,
-      imageurl: "../assets/Img/hot5.jpeg",
+      imageurl: "../assets/Img/hot5.jpg",
       name: "Calabresa",
       price: "R$11,99"
   },
   {
       id: 6,
-      imageurl: "../assets/Img/hot6.jpeg",
+      imageurl: "../assets/Img/hot6.jpg",
       name: "Cachorro Quente com Pedaços de Carne",
       price: "R$15,99"
   },
   {
       id: 7,
-      imageurl: "../assets/Img/hot7.jpeg",
+      imageurl: "../assets/Img/hot7.jpg",
       name: "Dogão",
       price: "R$16,99"
   },
   {
       id: 8,
-      imageurl: "../assets/Img/hot8.jpeg",
+      imageurl: "../assets/Img/hot8.jpg",
       name: "Cachorro Quente Insano",
       price: "R$18,99"
   }
@@ -120,49 +120,49 @@ export const menuHotD = [
   export const menuPorcao = [
     {
       id: 1,
-      imageurl: "../assets/Img/port1.jpeg",
+      imageurl: "../assets/Img/port1.jpg",
       name: "Aneis de Cebola",
       price: "R$18,99"
   },
   {
       id: 2,
-      imageurl: "../assets/Img/port2.jpeg",
+      imageurl: "../assets/Img/port2.jpg",
       name: "Mix",
       price: "R$23,50"
   },
   {
       id: 3,
-      imageurl: "../assets/Img/port3.jpeg",
+      imageurl: "../assets/Img/port3.jpg",
       name: "Camarão",
       price: "R$30,00"
   },
   {
       id: 4,
-      imageurl: "../assets/Img/port4.jpeg",
+      imageurl: "../assets/Img/port4.jpg",
       name: "Costelinha",
       price: "R$25,75"
   },
   {
       id: 5,
-      imageurl: "../assets/Img/port5.jpeg",
+      imageurl: "../assets/Img/port5.jpg",
       name: "Coxa de frango",
       price: "R$15,99"
   },
   {
       id: 6,
-      imageurl: "../assets/Img/port6.jpeg",
+      imageurl: "../assets/Img/port6.jpg",
       name: "Batata com bacon",
       price: "R$20,00"
   },
   {
       id: 7,
-      imageurl: "../assets/Img/port7.jpeg",
+      imageurl: "../assets/Img/port7.jpg",
       name: "File de peixe",
       price: "R$28,50"
   },
   {
       id: 8,
-      imageurl: "../assets/Img/port8.jpeg",
+      imageurl: "../assets/Img/port8.jpg",
       name: "Torresminho",
       price: "R$18,00"
   }
